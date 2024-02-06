213e576a1a3a004c82c69dfbd294ba588c2e5189
Luca <zamboluca@gmail.com>
2024-02-06 22:51:36 +0100

    improve: use PySide6 QMessageBox.Yes as rvalue

--- doorhole.py
+++ doorhole.py
@@ -377,7 +377,7 @@ class RequirementSetModel(QAbstractTableModel):
 			qm.setText(str("This will delete the requirement from disk.\nYou will not be able to recover it unless it's versioned.\n\nAre you absolutely sure?"))
 			qm.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
 			ret = qm.exec()
-			if ret == QMessageBox.StandardButtons.Yes.value:
+			if ret == QMessageBox.Yes:
 				self.delReq(row)
 	
 	def getItem(self, qidx):
